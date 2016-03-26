405c793708b4ad43d9d01652eba88bc178c07f81
marzim <marvin.casagnap@gmail.com>
2016-03-26 10:32:06 +0800

    commit comments

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -29,6 +29,9 @@ Rails.application.routes.draw do
   #     end
   #   end
 
+  resources :articles do
+    resources :comments
+  end
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
